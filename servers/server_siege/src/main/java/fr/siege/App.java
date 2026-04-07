f2cabe476982fbe9b0a4866cf7ef5724c9da217d
Lucas Priez <priez.lucas80@gmail.com>
2026-04-07 17:20:50 +0200

    feat(UpdateBill): Imp

--- servers/server_siege/src/main/java/fr/siege/App.java
+++ servers/server_siege/src/main/java/fr/siege/App.java
@@ -1,6 +1,9 @@
 package fr.siege;
 
 import java.rmi.registry.Registry;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import fr.siege.interfaces.IHeptathlon_siege;
 
@@ -16,8 +19,22 @@ public class App {
             .exportObject(heptathlonSiege, 1099);
 
             registry.bind("HeptathlonSiege", stub);
-
             System.out.println("Heptathlon Siege server is ready.");
+
+            System.out.println("Starting cron job to update prices...");
+
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            scheduler.scheduleAtFixedRate(() -> {
+                System.out.println("Updating prices...");
+                try {
+                    heptathlonSiege.updatePrices(1+0.1);
+                    System.out.println("Prices updated successfully.");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println("Failed to update prices: " + e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            }, 1, 1, TimeUnit.MINUTES);
 
 
         } catch (Exception e) {
